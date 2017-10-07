d032b9f1d9e53ca467b8578af45ed901d32de46f
Justin <justinyang339@yahoo.com>
2017-10-07 16:52:00 +0800

    implemeneted the kvStore, and part of kvServer. Need to parse request and then send back response to clients

--- src/github.com/cmu440/p0/kv_impl.go
+++ src/github.com/cmu440/p0/kv_impl.go
@@ -2,21 +2,36 @@
 
 package p0
 
-var kvstore = make(map[string][]byte)
+/*
+Go doesn't have the concept of class, use struct instead
+accee modifier: there is public/private/protected keyword. use*/
+/*small/big initials: if the first letter is big case, it is exported*/
+/*otherwise it is only package access
+
+Presence of interface: to enable polymorphism
+*/
+
+type KVStore struct{
+	/* can struct fields be initialized in definition */
+	var kvstore map[string][]byte
+}
+
 
 // this function instantiates the database
-func init_db() {
-    kvstore = make(map[string][]byte)
+func (kvstore *KVStore) init_db() {
+	/* []byte is a byte array which is type as well*/
+	kvstore = make(map[string][]byte)
 }
 
 // put inserts a new key value pair or updates the value for a
 // given key in the store
-func put(key string, value []byte) {
-    kvstore[key] = value
+func (kvstore *KVStore) put(key string, value []byte) {
+	/* kvstore is a pointer, can it be used in this way? yes*/
+	kvstore[key] = value
 }
 
 // get fetches the value associated with the key
-func get(key string) []byte {
-    v, _ := kvstore[key]
-    return v
+func (kvstore *KVStore) get(key string) []byte {
+	v, _ := kvstore[key]
+	return v
 }
--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,77 @@
 
 package p0
 
-type keyValueServer struct {
-    // TODO: implement this!
+import {
+	"fmt"
+	"net"
+	"bufio"
+	"bytes"
+	"strconv"
 }
 
+type keyValueServer struct {
+	var kvstore KVStore
+	var conns int
+}
+
+const (
+	CONN_HOST = "localhost"
+	CONN_TYPE = "tcp"
+	BUFFER_SIZE = 1024
+)
+
+
+
 // New creates and returns (but does not start) a new KeyValueServer.
-func New() KeyValueServer {
-    // TODO: implement this!
-    return nil
+func New() KeyValueServer *keyValueServer{
+	kvServer := &{conns: 0, kvstore: make(KVStore)}
+	kvServer.kvstore.init_db()
+	return kvServer
 }
 
 func (kvs *keyValueServer) Start(port int) error {
-    // TODO: implement this!
-    return nil
+	l, err = net.Listen(CONN_TYPE, CONN_HOST + ":" + string(port))
+	if err != nil {
+		/* log network error to stdio*/
+		fmt.Println("Error listening:", err.Error())
+		return err
+	}
+	/* when existing from this function, close the port */
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST  + ":" + string(port))
+	for { // Listen for an incoming connection
+		conn, err = l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			/* does this keyword exist ?*/
+			continue
+		}
+		// Handle the new connection in a new goroutine
+		go handleRequest(conn)
+	}
+	return nil
 }
 
 func (kvs *keyValueServer) Close() {
-    // TODO: implement this!
+	// TODO: release the kv store; how to terminate all goroutines?
 }
 
 func (kvs *keyValueServer) Count() int {
-    // TODO: implement this!
-    return -1
+	// TODO: return the # of connected clients
+	return -1
 }
 
 // TODO: add additional methods/functions below!
+
+/* this function will handle newly accepted connections in a go */
+/* routine */
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, BUFFER_SIZE)
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return
+	}
+
+	/* todo: need to parse request and send back response */
+}
